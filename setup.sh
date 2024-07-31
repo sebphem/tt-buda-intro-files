b502a1808d7e0286830e347d4fb4c6576c6bfbc6
sebastian-phemister <sebastian.phemister@gmail.com>
2024-07-31 08:44:17 -0700

    add setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# run these commands to setup a mini environment for this demo
+python -m venv
+source venv/bin/activate
+
+# install all the reqs, you can do this either in the virtual environment or in your global install
+pip install -r requirements.txt
